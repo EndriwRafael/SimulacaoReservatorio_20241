39d205a02bddfebaa52ffebaaf824d1c860cdeb1
EndriwRafael <endriwrafaell@gmail.com>
2024-03-09 18:11:08 -0300

    Correção do posicionamento de algumas funções. Porém ainda não está completo!

--- Main_.py
+++ Main_.py
@@ -1,18 +1,22 @@
 """
 Simulador Fluxo Linear Monofásico - 1D
 """
-import matplotlib.pyplot as plt
+import Simuladores as sm
 import numpy as np
-from Simuladores import *
 
-
-pressure_initial = 100
-pressure_well = 50
-length_reser = 500
+pressure_initial = 20000
+pressure_well = 18000
+length_reser = 5000
 permeabi = 200
 porosit = 0.2
-viscosi = .5
-compressibi = 10e-6
+viscosi = .005
+compressibi = 10e-5
 
-SimuladorPressPress(initialpressure=pressure_initial, wellpressure=pressure_well, reserlength=length_reser,
-                    permeability=permeabi, viscosity=viscosi, porosity=porosit, compresibility=compressibi)
+data_case = sm.SimuladorPressPress(initialpressure=pressure_initial, wellpressure=pressure_well,
+                                   reserlength=length_reser,
+                                   permeability=permeabi, viscosity=viscosi, porosity=porosit,
+                                   compresibility=compressibi)
+
+x = np.linspace(start=0, stop=data_case.res_length, num=100)
+t = np.linspace(start=0, stop=10, num=11)
+data_case.start_simulation(x=x, t=t, deltapressure=data_case.deltaPressure, wellpressure=data_case.well_pressure)
--- Simuladores.py
+++ Simuladores.py
@@ -11,12 +11,39 @@ class SimuladorPressPress:
         self.viscosity = viscosity
         self.porosity = porosity
         self.compressibility = compresibility
-        self.result = self.calc()
-        self.plot_result()
+        self.eta, self.deltaPressure = self.calc()
 
-    def calc(self) -> int:
+    def calc(self) -> tuple[float, float]:
         delta_pressure = self.initial_pressure - self.well_pressure
-        return delta_pressure
+        eta = self.permeability / (self.viscosity * self.compressibility * self.porosity)
+        return eta, delta_pressure
 
-    def plot_result(self):
-        print(self.result)
+    def calc_sum(self, time, point):
+        if time == 0 or point == 0:
+            sum_value = 0
+        else:
+            sum_value = 0
+            n = 1
+            erro = 100
+            eppara = 0.001
+            while erro >= eppara:
+                sum_old = sum_value
+                sum_value += ((np.exp(-1 * (((n * np.pi) / self.res_length) ** 2) * (self.eta * time)) / n) * np.sin(
+                    (n * np.pi * point)
+                    / self.res_length))
+                erro = np.fabs(sum_value - sum_old)/sum_value * 100
+                n += 1
+        return sum_value
+
+    def start_simulation(self, x: np.ndarray, t: np.ndarray, deltapressure: float, wellpressure: float):
+        pressure = {}
+        for time in t:
+            vetor_for_time = []
+
+            for point in x:
+                suma = self.calc_sum(time, point)
+                vetor_for_time.append(deltapressure * suma + wellpressure)
+
+            pressure['time'] = vetor_for_time
+
+        return pressure
