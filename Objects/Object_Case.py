42186383cf926967ada0a84bdfac66864d9ea342
EndriwRafael <endriwrafaell@gmail.com>
2024-06-08 16:25:18 -0300

    Implementation of One and Two dimensional objects that will compute the post process analysis.
    - Remove the condiction objects that early was one for each condiction boundaries!
    
    - Now we have just one object for each flux type (1D, 2D or 3D)!

--- Objects/Object_Case.py
+++ Objects/Object_Case.py
@@ -45,21 +45,24 @@ class ObjectCase(ABC):
     @abstractmethod
     def compute(self):
         """
-        Method to compute the field pressure for the problem
-        Returns the results simulation for all methods (explicit and implicit)
+        Method to compute the field pressure error for the problem methods used compared with analitical method.
+        ----------------------------------------------------------------------------------------------------------------
         """
         pass
 
 
-class PressureBoundaries(ObjectCase):
-    def __init__(self):
+class OneDimensionalFlowCase(ObjectCase):
+    def __init__(self, condiction):
         super().__init__()
-        self.condiction = 'PP'
+        self.condiction = condiction
+        self.rootpath = f'results\\OneDimensionalFlow\\{self.condiction}'
+        self.createpath()
+
+    def createpath(self):
+        if not os.path.isdir(f'.\\{self.rootpath}'):
+            os.makedirs(f'.\\{self.rootpath}')
 
     def compute(self):
-        if not os.path.isdir('../results/OneDimensionalFlow/PressurePressure_Simulator'):
-            os.makedirs('../results/OneDimensionalFlow/PressurePressure_Simulator')
-        # --------------------------------------------------------------------------------------------------------------
         results = self.results
         extract_columns = [col for col in results.implicit.columns if col != 0.0 and col in results.explicit.columns]
 
@@ -87,83 +90,23 @@ class PressureBoundaries(ObjectCase):
         # --------------------------------------------------------------------------------------------------------------
         data = Functions.create_errordataframe_1d(explit_list=list_explicit, implicit_list=list_implicit,
                                                   columns=extract_columns)
-        data.to_excel(r'results\OneDimensionalFlow\PressurePressure_Simulator\Error_analysis.xlsx')
-        root_fig = r'results\OneDimensionalFlow\PressurePressure_Simulator'
-        Functions.plot_graphs(dataclass=results, columns=[10, 50, 100], root=root_fig)
-        Functions.plot_animation_results(data=results, root=root_fig)
-        Functions.plot_pressuremap_animation(data=results, root=root_fig)
+        data.to_excel(f'{self.rootpath}\\Error_analysis.xlsx')
+
+        Functions.plot_graphs(dataclass=results, columns=[10, 50, 100], root=self.rootpath)
+        Functions.plot_animation_results(data=results, root=self.rootpath)
+        Functions.plot_pressuremap_animation(data=results, root=self.rootpath)
 
 
-class FlowPressureBoundaries(ObjectCase):
-    def __init__(self):
+class TwoDimensionalFlowCase(ObjectCase):
+    def __init__(self, condiction):
         super().__init__()
-        self.condiction = 'FP'
+        self.condiction = condiction
+        self.rootpath = f'results\\TwoDimensionalFlow\\{self.condiction}'
+        self.createpath()
+
+    def createpath(self):
+        if not os.path.isdir(f'.\\{self.rootpath}'):
+            os.makedirs(f'.\\{self.rootpath}')
 
     def compute(self):
-        if not os.path.isdir('../results/OneDimensionalFlow/FlowPressure_Simulator'):
-            os.makedirs('../results/OneDimensionalFlow/FlowPressure_Simulator')
-        # --------------------------------------------------------------------------------------------------------------
-        results = self.results
-        extract_columns = [col for col in results.implicit.columns if col != 0.0 and col in results.explicit.columns]
-        # --------------------------------------------------------------------------------------------------------------
-        erro_explicit_dx, erro_explicit_dt = Functions.fo_erro(data_analitical=results.analitical_explicit,
-                                                               data_method=results.explicit,
-                                                               columns=extract_columns, n_cell=results.ncell_explicit)
-        erro_implicit_dx, erro_implicit_dt = Functions.fo_erro(data_analitical=results.analitical_implicit,
-                                                               data_method=results.implicit,
-                                                               columns=extract_columns, n_cell=results.ncell_implicit)
-        # --------------------------------------------------------------------------------------------------------------
-        list_explicit = [results.dx_explicit, results.time_explicit[1] - results.time_explicit[0],
-                         results.ncell_explicit, results.timeprocess_explicit]
-        for i in erro_explicit_dx:
-            list_explicit.append(i)
-        for i in erro_explicit_dt:
-            list_explicit.append(i)
-
-        list_implicit = [results.dx_implicit, results.time_implicit[1] - results.time_implicit[0],
-                         results.ncell_implicit, results.timeprocess_implicit]
-        for i in erro_implicit_dx:
-            list_implicit.append(i)
-        for i in erro_implicit_dt:
-            list_implicit.append(i)
-        # --------------------------------------------------------------------------------------------------------------
-        data = Functions.create_errordataframe_1d(explit_list=list_explicit, implicit_list=list_implicit,
-                                                  columns=extract_columns)
-        data.to_excel(r'results\OneDimensionalFlow\FlowPressure_Simulator\Error_analysis.xlsx')
-        root_fig = r'results\OneDimensionalFlow\FlowPressure_Simulator'
-        Functions.plot_graphs(dataclass=results, columns=[10, 50, 100], root=root_fig)
-        Functions.plot_animation_results(data=results, root=root_fig)
-        Functions.plot_pressuremap_animation(data=results, root=root_fig)
-
-
-class FlowBoundaries(ObjectCase):
-    def __init__(self):
-        super().__init__()
-        self.condiction = 'FF'
-
-    def compute(self):
-        if not os.path.isdir('results/OneDimensionalFlow/FlowFlow_Simulator'):
-            os.makedirs('results/OneDimensionalFlow/FlowFlow_Simulator')
-        # --------------------------------------------------------------------------------------------------------------
-        results = self.results
-        extract_columns = [col for col in results.implicit.columns if col != 0.0 and col in results.explicit.columns]
-        # --------------------------------------------------------------------------------------------------------------
-        erro_explicit = Functions.fo_erro(data_analitical=results.analitical_explicit, data_method=results.explicit,
-                                          columns=extract_columns, n_cell=results.ncell_explicit)
-        erro_implicit = Functions.fo_erro(data_analitical=results.analitical_implicit, data_method=results.implicit,
-                                          columns=extract_columns, n_cell=results.ncell_implicit)
-        # --------------------------------------------------------------------------------------------------------------
-        list_explicit = [results.dx_explicit, results.time_explicit[1] - results.time_explicit[0],
-                         results.ncell_explicit, results.timeprocess_explicit]
-        for i in erro_explicit:
-            list_explicit.append(i)
-
-        list_implicit = [results.dx_implicit, results.time_implicit[1] - results.time_implicit[0],
-                         results.ncell_implicit, results.timeprocess_implicit]
-        for i in erro_implicit:
-            list_implicit.append(i)
-        # --------------------------------------------------------------------------------------------------------------
-        data = Functions.create_errordataframe_1d(explit_list=list_explicit, implicit_list=list_implicit,
-                                                  columns=extract_columns)
-        data.to_excel('results/OneDimensionalFlow/FlowFlow_Simulator/Error_analysis.xlsx')
-        pass
+        return
