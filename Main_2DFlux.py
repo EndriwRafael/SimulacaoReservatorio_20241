512e8015ef91469cb77aac88af1773f3973661c1
EndriwRafael <endriwrafaell@gmail.com>
2024-06-17 18:22:56 -0300

    Posição e identificação dos poços:
    O parâmetro well_positions será um dado de entrada para os poços selecionados. Sua estrutura básica consiste nas chaves principais que são os poços selecionados, onde cada chave consiste também de um dicionário composto pelas chaves secundárias (Position: posição i e j do poço na malha, e Type: se o poço é produtor ou injetor).

--- Main_2DFlux.py
+++ Main_2DFlux.py
@@ -1,6 +1,8 @@
 """
 Main script to simulate Bilinear (2D) flow Monofase - Heterogeneous and homogeneous environments
 """
+import pandas as pd
+
 import Functions
 import numpy as np
 
@@ -21,6 +23,7 @@ qleft = 0
 qtop = 0
 qbase = 0
 qright = 0
+permeability_map = 'PermeabilityMaps/Permeability_Map_With_DiagonalFault.txt'
 
 ''' Definindo condições de simulação ------------------------------------------------------------------------------- '''
 # Para fluxo 1D:
@@ -35,21 +38,21 @@ base_boundary = 'F'
 flux = '2D'
 
 well_positions = {
-    'well 1': ()
+    'well 1': {'Position': (35, 60), 'Type': 'Production'}
 }
 
 # Dados para criação de malha - O número de células deve ser igual, para x e y.
 time_implicit = np.linspace(start=0, stop=100, num=101)
-n_cells_implicit = 1000
+n_cells_implicit = 100
 
 ''' Inicializando simulador ---------------------------------------------------------------------------------------- '''
 case = Functions.get_object_case(top_condiction=top_boundary, base_condiction=base_boundary, fluxtype=flux,
                                  left_condition=left_boundary, right_condition=right_boundary)
 
 case.set_case_parameters(initial_press=pressure_initial, well_press=pressure_well, res_area=area, res_thick=thickness,
-                         porosity=porosit, viscosity=viscosi, compressibility=compressibi, permeability=permeabi,
-                         res_len=length_reser_x, res_width=length_reser_y, baseflow=qbase, leftflow=qleft,
-                         rightflow=qright, topflow=qtop)
+                         porosity=porosit, viscosity=viscosi, compressibility=compressibi,
+                         permeability=permeability_map, res_len=length_reser_x, res_width=length_reser_y,
+                         baseflow=qbase, leftflow=qleft, rightflow=qright, topflow=qtop, well_position=well_positions)
 
 ''' Criando malhas ------------------------------------------------------------------------------------------------- '''
 simulation = Functions.get_object_mesh(flow_type=flux, wellobject=case)
